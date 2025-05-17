ccfd7f55b79fe0465eab24c8d0ad7f51907b6b52
Abdallah Mohammed <abdallahabdulzahir@gmail.com>
2025-05-17 17:11:13 +0300

    Initial Commit

---
+++ ChatViewApp/ChatViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewAppApp.swift
+//  ChatViewApp
+//
+//  Created by name on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatViewApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatViewApp
+//
+//  Created by name on 17/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
